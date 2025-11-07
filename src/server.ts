397dfe51bf0d49f7ce2f26c65f953611adeaae8e
Rajput-Suraj <surajarajput@gmail.com>
2025-11-07 12:52:36 +0530

    Add express server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,16 @@
-const helloWorld = () => {
-  return "Hello World";
+import app from "./app.ts";
+import { Config } from "./config/index.ts";
+
+const startServer = () => {
+  const PORT = Config.PORT;
+  try {
+    // eslint-disable-next-line no-console
+    app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+    process.exit(1);
+  }
 };
 
-helloWorld();
+startServer();
