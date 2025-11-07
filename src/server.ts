be314f467e996476fcf266e1637cf659b010f1bb
Rajput-Suraj <surajarajput@gmail.com>
2025-11-07 13:05:44 +0530

    Add winston logger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import app from "./app.ts";
+import logger from "./config/logger.ts";
 import { Config } from "./config/index.ts";
 
 const startServer = () => {
   const PORT = Config.PORT;
   try {
-    // eslint-disable-next-line no-console
-    app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+    app.listen(PORT, () =>
+      logger.log("info", `Server is listening on port ${PORT}`),
+    );
   } catch (err) {
     // eslint-disable-next-line no-console
     console.error(err);
