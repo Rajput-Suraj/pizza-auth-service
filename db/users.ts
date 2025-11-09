7f02b48bca7df6e8a5b9735633adb1f1ba8675a7
Rajput-Suraj <surajarajput@gmail.com>
2025-11-09 14:12:00 +0530

    Add code to add user to DB without auth

--- db/users.ts
+++ db/users.ts
@@ -1,5 +1,8 @@
-import { integer, pgTable } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  firstName: varchar({ length: 255 }).notNull(),
+  lastName: varchar({ length: 255 }).notNull(),
+  email: varchar().unique().notNull(),
 });
--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,17 +1,21 @@
 import request from "supertest";
 
 import app from "../../src/app";
+import db from "../../db/index";
+import { usersTable } from "../../db/users";
+
+const userData = {
+  firstName: "Suraj",
+  lastName: "Rajput",
+  email: "example@gmail.com",
+  password: "123456",
+};
 
 describe("POST /auth/register", () => {
   describe("Given all fields", () => {
     it("should return the 201 status code", async () => {
       // Arrange
-      const userData = {
-        firstName: "Suraj",
-        lastName: "Rajput",
-        email: "example@gmail.com",
-        password: "123456",
-      };
+      // Add userData here
       // Act
       const response = await request(app).post("/auth/register").send(userData);
       // Assert
@@ -19,19 +23,17 @@ describe("POST /auth/register", () => {
     });
 
     it("should return valid JSON response", async () => {
-      // Arrange
-      const userData = {
-        firstName: "Suraj",
-        lastName: "Rajput",
-        email: "example@gmail.com",
-        password: "123456",
-      };
-      // Act
       const response = await request(app).post("/auth/register").send(userData);
       // Assert
       expect(
         (response.headers as Record<string, string>)["content-type"],
       ).toEqual(expect.stringContaining("json"));
+    });
+
+    it("should persist the user in the database", async () => {
+      const res = await db.select().from(usersTable);
+
+      expect(res).toHaveLength(1);
     });
   });
   describe("Fields are missing", () => {});
