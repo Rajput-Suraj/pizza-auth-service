3ecb49a360ab7c9289d1613922eeb4c6d85a1d00
Rajput-Suraj <surajarajput@gmail.com>
2025-11-11 19:59:38 +0530

    Add role column to users table

--- src/db/schemas/users.ts
+++ src/db/schemas/users.ts
@@ -5,5 +5,6 @@ export const usersTable = pgTable("users", {
   firstName: varchar({ length: 255 }).notNull(),
   lastName: varchar({ length: 255 }),
   email: varchar({ length: 25 }).notNull().unique(),
+  role: varchar("role", { length: 50 }),
   created_at: timestamp("created_at").defaultNow().notNull(),
 });
