f1c007e3fb6bdd2eb80c4a9f1e8a10cae70151dc
Rajput-Suraj <surajarajput@gmail.com>
2025-11-19 07:20:04 +0530

    Deleted many-to-many relation

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,3 @@
-export * from "./relations.ts";
 export * from "./schemas/users.ts";
 export * from "./schemas/tenant.ts";
 export * from "./schemas/refreshToken.ts";
-export * from "./schemas/usersToTenants.ts";
