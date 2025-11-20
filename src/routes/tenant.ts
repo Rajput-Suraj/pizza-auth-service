45cf262c9ab5a445e8abd15ef269a80a5ff6c1b4
Rajput-Suraj <surajarajput@gmail.com>
2025-11-20 07:09:43 +0530

    Add middleware to protect the route

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -2,6 +2,7 @@ import express from "express";
 import type { NextFunction, Request, Response } from "express";
 
 import logger from "../config/logger.ts";
+import authenticate from "../middlewares/authenticate.ts";
 import { TenantService } from "../services/TenantService.ts";
 import { TenantController } from "../controllers/TenantController.ts";
 
@@ -9,8 +10,11 @@ const router = express.Router();
 const tenantService = new TenantService();
 const tenantController = new TenantController(tenantService, logger);
 
-router.post("/", (req: Request, res: Response, next: NextFunction) =>
-  tenantController.create(req, res, next),
+router.post(
+  "/",
+  authenticate,
+  (req: Request, res: Response, next: NextFunction) =>
+    tenantController.create(req, res, next),
 );
 
 export default router;
