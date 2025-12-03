a3c47022b32ae6d52d2ac7a4f7a409721fc6b66c
Rajput-Suraj <surajarajput@gmail.com>
2025-12-03 12:01:07 +0530

    Removed more console logs

--- scripts/generateKeys.js
+++ scripts/generateKeys.js
@@ -13,8 +13,5 @@ const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
   },
 });
 
-console.log("Public Key", publicKey);
-console.log("Private Key", privateKey);
-
 fs.writeFileSync("certs/public.pem", publicKey);
 fs.writeFileSync("certs/private.pem", privateKey);
