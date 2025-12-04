167b31600e8a837b35483e2c5ec805fbe5f9d6ab
Rajput-Suraj <surajarajput@gmail.com>
2025-12-04 07:05:53 +0530

    Updated test cases

--- src/demo.spec.ts
+++ src/demo.spec.ts
@@ -1,13 +1,8 @@
 import request from "supertest";
 
-import { add } from "./server.ts";
 import app from "./app.ts";
 
-describe.skip("Add", () => {
-  it("adds two numbers", () => {
-    expect(add(1, 2)).toBe(3);
-  });
-
+describe.skip("App", () => {
   it("Should work", async () => {
     const res = await request(app).get("/").send();
     expect(res.statusCode).toBe(200);
